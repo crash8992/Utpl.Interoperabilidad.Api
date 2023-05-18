c6b1c2b53e6177a20a00a6e0d19a0bc9a16d7bb2
Cristian Bravo <crash899@gmail.com>
2023-05-18 02:10:34 +0000

    agregar base de datos

--- main.py
+++ main.py
@@ -2,32 +2,69 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 
+import sqlite3
+
 app = FastAPI()
 
-class Persona (BaseModel):
+conn = sqlite3.connect('databaseCB.db')
+cursor = conn.cursor()
+
+class Jugador(BaseModel):
     id: int
     nombre: str
     edad: int
-    ciudad: Optional[str] = None
+    equipo: Optional[str] = None
+    minutos_jugados: Optional[float] = None
 
-personaList = []
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS jugadores (
+        id INTEGER PRIMARY KEY,
+        nombre TEXT,
+        edad INTEGER,
+        equipo TEXT,
+        minutos_jugados REAL
+    )
+'''
+cursor.execute(create_table_query)
+conn.commit()
 
-@app.post("/personas", response_model=Persona)
-def crear_persona(person: Persona):
-    personaList.append(person)
-    return person
+@app.post("/jugadores", response_model=Jugador)
+def crear_jugador(jugador: Jugador):
+    insert_query = '''
+        INSERT INTO jugadores (id, nombre, edad, equipo, minutos_jugados)
+        VALUES (?, ?, ?, ?, ?)
+    '''
+    cursor.execute(insert_query, (jugador.id, jugador.nombre, jugador.edad, jugador.equipo, jugador.minutos_jugados))
+    conn.commit()
+    return jugador
 
-@app.get("/personas", response_model=List[Persona])
-def get_personas():
-    return personaList
+@app.get("/jugadores", response_model=List[Jugador])
+def get_jugadores():
+    select_query = '''
+        SELECT id, nombre, edad, equipo, minutos_jugados
+        FROM jugadores
+    '''
+    cursor.execute(select_query)
+    jugadores = []
+    for row in cursor.fetchall():
+        id, nombre, edad, equipo, minutos_jugados = row
+        jugadores.append(Jugador(id=id, nombre=nombre, edad=edad, equipo=equipo, minutos_jugados=minutos_jugados))
+    return jugadores
 
-@app.get("/personas/{persona_id}", response_model=Persona)
-def obtener_persona (persona_id: int):
-    for persona in personaList:
-        if persona.id == persona_id:
-            return persona
-    raise HTTPException(status_code=404, detail="Persona no encontrada")
+@app.get("/jugadores/{jugador_id}", response_model=Jugador)
+def obtener_jugador(jugador_id: int):
+    select_query = '''
+        SELECT id, nombre, edad, equipo, minutos_jugados
+        FROM jugadores
+        WHERE id = ?
+    '''
+    cursor.execute(select_query, (jugador_id,))
+    jugador = cursor.fetchone()
+    if jugador:
+        id, nombre, edad, equipo, minutos_jugados = jugador
+        return Jugador(id=id, nombre=nombre, edad=edad, equipo=equipo, minutos_jugados=minutos_jugados)
+    raise HTTPException(status_code=404, detail="Jugador no encontrado")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "buenas noches"}
+@app.on_event("shutdown")
+def shutdown_event():
+    conn.close()
