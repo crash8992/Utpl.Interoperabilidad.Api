adc46a88fe97e0bf150bb27dc248e4d113b0b067
Cristian Bravo <crash899@gmail.com>
2023-06-15 01:55:19 +0000

    cambios code

--- main.py
+++ main.py
@@ -62,18 +62,18 @@ def crear_jugar(jugador: Jugar):
     jugarList.append(jugador)
     return jugador
 
-@app.get("/jugadores", response_model=List[Jugar], tags = ["personas"])
+@app.get("/jugadores", response_model=List[Jugar], tags = ["jugadores"])
 def get_jugadores():
     return jugarList
 
-@app.get("/jugadores/{jugador_id}", response_model=Jugar, tags = ["personas"])
+@app.get("/jugadores/{jugador_id}", response_model=Jugar, tags = ["jugadores"])
 def obtener_jugador(jugador_id: int):
     for jugador in jugarList:
         if jugador.id == jugador_id:
             return jugador
     raise HTTPException(status_code=404, detail="Jugador no encontrado")
 
-@app.delete("/jugadores/{jugador_id}", tags = ["personas"])
+@app.delete("/jugadores/{jugador_id}", tags = ["jugadores"])
 def eliminar_jugador(jugador_id: int):
     for index, jugador in enumerate(jugarList):
         if jugador.id == jugador_id:
