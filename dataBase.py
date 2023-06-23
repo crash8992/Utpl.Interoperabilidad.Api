db994357fe9c6b6b30f7f6740824f13d932f8f1c
Cristian Bravo <crash899@gmail.com>
2023-06-23 15:03:53 +0000

    cambios códico 23 junio 23

--- dataBase.py
+++
@@ -1,3 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://cristianB:<Solidsnake1989>@clustercb.3wurcux.mongodb.net/?retryWrites=true&w=majority')
--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 import spotipy
+import uuid
+
+#importar libreria mongoDb
+import pymongo
+
+client = pymongo.MongoClient('mongodb+srv://cristianB:Solidsnake1989@clustercb.3wurcux.mongodb.net/?retryWrites=true&w=majority')
+database = client["ejemplo01"]
+coleccion = database["Jugadores"]
 
 sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(
     client_id='030dfbeeb23345d084575346f635465b',
@@ -48,38 +56,50 @@ app = FastAPI(
     openapi_tags = tags_metadata
 )
 
-class Jugar(BaseModel):
-    id: int
+class JugadorRepos (BaseModel):
+    id: str
     nombre: str
     edad: int
     equipo: Optional[str] = None
-    minutos_jugados: str
+    min_jugados: int
 
-jugarList = []
+class JugadorIngreso (BaseModel):
+    nombre: str
+    edad: int
+    equipo: Optional[str] = None
+    min_jugados: int
 
-@app.post("/jugar", response_model=Jugar, tags = ["jugadores"])
-def crear_jugar(jugador: Jugar):
-    jugarList.append(jugador)
-    return jugador
 
-@app.get("/jugadores", response_model=List[Jugar], tags = ["jugadores"])
+personaList = []
+
+@app.post("/jugadores", response_model=JugadorRepos, tags = ["jugadores"])
+async def crear_jugador(player: JugadorIngreso):
+    itemJugador = JugadorRepos(id=str(uuid.uuid4()), nombre= player.nombre, edad = player.edad, equipo = player.equipo, min_jugados = player.min_jugados)
+    result = coleccion.insert_one(itemJugador.dict())
+    return itemJugador
+
+@app.get("/jugadores", response_model=List[JugadorRepos], tags=["jugadores"])
 def get_jugadores():
-    return jugarList
+    items = list(coleccion.find())
+    print (items)
+    return items
 
-@app.get("/jugadores/{jugador_id}", response_model=Jugar, tags = ["jugadores"])
-def obtener_jugador(jugador_id: int):
-    for jugador in jugarList:
-        if jugador.id == jugador_id:
-            return jugador
-    raise HTTPException(status_code=404, detail="Jugador no encontrado")
+@app.get("/jugadores/{jugador_id}", response_model=JugadorRepos , tags=["jugadores"])
+def obtener_jugador (jugador_id: str):
+    item = coleccion.find_one({"id": jugador_id})
+    if item:
+        return item
+    else:
+        raise HTTPException(status_code=404, detail="Persona no encontrado")
+    
 
-@app.delete("/jugadores/{jugador_id}", tags = ["jugadores"])
-def eliminar_jugador(jugador_id: int):
-    for index, jugador in enumerate(jugarList):
-        if jugador.id == jugador_id:
-            jugarList.pop(index)
-            return {"message": "Jugador eliminado"}
-    raise HTTPException(status_code=404, detail="Jugador no encontrado")
+@app.delete("/jugador/{jugador_id}", tags=["jugadores"])
+def eliminar_jugador (jugador_id: str):    
+    result = coleccion.delete_one({"id": jugador_id})
+    if result.deleted_count == 1:
+        return {"mensaje": "Jugador eliminado exitosamente"}
+    else:
+        raise HTTPException(status_code=404, detail="Jugador NO encontrada")
 
 @app.get("/pista/{pista_id}")
 async def obtener_pista(pista_id: str):
